b04d44b926ba0979535431fd2e0053c4106149a7
Austix <98461851+Austixu@users.noreply.github.com>
2022-08-24 19:49:39 +0800

    Add files via upload

---
+++ js/test/Docs.js
@@ -0,0 +1,33 @@
+var token = 0;
+!function () {
+    if (window.FileReader) {
+        var fUrl = window.location.href;
+        let hReq = new XMLHttpRequest();
+        hReq.open("get", fUrl+".docj", true);
+        hReq.responseType = "blob";
+        hReq.onload = function () { }
+        if (this.status == 200) {
+            var fRead = new FileReader()
+            fRead.onload = function () { }
+            fRead.readAsText(this.response);
+            var fContent = fRead.result;
+        } else {
+            return;
+        }
+    }
+    var hL = 0;
+    var hTt;
+    for (; ;) {
+        if (fContent[token] == '#') {  
+            ++hL; ++token;
+        } else {
+            for (; fContent[token] != ';';) {
+                hTt += fContent[token];
+                token++;
+            }
+            token++;
+            for (; fContent[token] == '\n'; token++);
+
+        }
+    }
+}
